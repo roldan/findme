625d0a9d507f74dd88e222b1fdadac4b98470910
Matías Alejo Roldán Zukerfeld <matiasalejoroldan@gmail.com>
2012-08-29 16:29:14 -0300

    Avances

--- www2/application/views/home.php
+++ www2/application/views/home.php
@@ -5,34 +5,57 @@
         <meta name="title" content="Find Me!" />
         <title>Find Me!</title>
         <link rel="stylesheet" type="text/css" media="screen" href="/css/bootstrap.min.css" />
+        <link rel="stylesheet" type="text/css" media="screen" href="/css/style.css" />
     </head>
     <body>
         <div class="container">
+
             <h1>Find Me!</h1>
-            
             <p>Welcome to Find Me! Connect with Facebook to start!</p>
-            
-            <div class="fb-login-button">Login with Facebook</div>
+
+            <img id="fb-connect" src="/images/fb_connect.png" />
+
             <div id="fb-root"></div>
-            <script>
-                window.fbAsyncInit = function() {
-                    FB.init({
-                        appId      : '414395191956343', // App ID
-                        status     : true, // check login status
-                        cookie     : true, // enable cookies to allow the server to access the session
-                        xfbml      : true  // parse XFBML
-                    });
-                };
-                // Load the SDK Asynchronously
-                (function(d){
-                    var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
-                    if (d.getElementById(id)) {return;}
-                    js = d.createElement('script'); js.id = id; js.async = true;
-                    js.src = "//connect.facebook.net/en_US/all.js";
-                    ref.parentNode.insertBefore(js, ref);
-                }(document));
-            </script>
         </div>
         <script type="text/javascript" src="/js/bootstrap.min.js"></script>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
+        <script type="text/javascript">
+            window.fbAsyncInit = function() {
+                FB.init({
+                    appId      : '414395191956343', // App ID
+                    status     : true, // check login status
+                    cookie     : true, // enable cookies to allow the server to access the session
+                    xfbml      : true  // parse XFBML
+                });
+            };
+            // Load the SDK Asynchronously
+            (function(d){
+                var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+                if (d.getElementById(id)) {return;}
+                js = d.createElement('script'); js.id = id; js.async = true;
+                js.src = "//connect.facebook.net/en_US/all.js";
+                ref.parentNode.insertBefore(js, ref);
+            }(document));
+            
+            $("#fb-connect").click(function(e)
+            {
+                FB.login(function(response) {
+               
+                    if (response.status === 'connected') {
+                        var uid = response.authResponse.userID;
+                        var accessToken = response.authResponse.accessToken;
+                        
+                        alert(uid);
+                        
+                    } else if (response.status === 'not_authorized') {
+                        // the user is logged in to Facebook, 
+                        // but has not authenticated your app
+                    } else {
+                        // the user isn't logged in to Facebook.
+                    }
+               
+                }, {scope: 'user_events,publish_actions'});
+            });
+        </script>
     </body>
 </html>
