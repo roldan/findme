0a9338ba7cc7d35f490bb12c272323846c642b9e
Matías Alejo Roldán Zukerfeld <matiasalejoroldan@gmail.com>
2012-08-29 16:14:10 -0300

    Avances sobre html

--- www2/application/views/home.php
+++ www2/application/views/home.php
@@ -3,11 +3,36 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <meta name="title" content="Find Me!" />
-        <title>Find Me! </title>
+        <title>Find Me!</title>
         <link rel="stylesheet" type="text/css" media="screen" href="/css/bootstrap.min.css" />
     </head>
     <body>
-        <h1>Hello, world!</h1>
+        <div class="container">
+            <h1>Find Me!</h1>
+            
+            <p>Welcome to Find Me! Connect with Facebook to start!</p>
+            
+            <div class="fb-login-button">Login with Facebook</div>
+            <div id="fb-root"></div>
+            <script>
+                window.fbAsyncInit = function() {
+                    FB.init({
+                        appId      : '414395191956343', // App ID
+                        status     : true, // check login status
+                        cookie     : true, // enable cookies to allow the server to access the session
+                        xfbml      : true  // parse XFBML
+                    });
+                };
+                // Load the SDK Asynchronously
+                (function(d){
+                    var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+                    if (d.getElementById(id)) {return;}
+                    js = d.createElement('script'); js.id = id; js.async = true;
+                    js.src = "//connect.facebook.net/en_US/all.js";
+                    ref.parentNode.insertBefore(js, ref);
+                }(document));
+            </script>
+        </div>
         <script type="text/javascript" src="/js/bootstrap.min.js"></script>
     </body>
 </html>
